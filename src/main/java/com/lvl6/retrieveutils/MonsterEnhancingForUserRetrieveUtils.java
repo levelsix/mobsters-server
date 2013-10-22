85642c3c664caa0e9de7d1dc1777ef4bec85954a
Arturo Wu Zhou <aweezy90@gmail.com>
2013-10-22 01:45:11 -0700

    timestamp retrieval

--- src/main/java/com/lvl6/retrieveutils/MonsterEnhancingForUserRetrieveUtils.java
+++ src/main/java/com/lvl6/retrieveutils/MonsterEnhancingForUserRetrieveUtils.java
@@ -24,95 +24,91 @@ import com.lvl6.utils.utilmethods.StringUtils;
 
 @Component @DependsOn("gameServer") public class MonsterEnhancingForUserRetrieveUtils {
 
-  private static Logger log = LoggerFactory.getLogger(new Object() { }.getClass().getEnclosingClass());
+	private static Logger log = LoggerFactory.getLogger(new Object() { }.getClass().getEnclosingClass());
 
-  private static final String TABLE_NAME = DBConstants.TABLE_MONSTER_ENHANCING_FOR_USER;
+	private static final String TABLE_NAME = DBConstants.TABLE_MONSTER_ENHANCING_FOR_USER;
 
 
-  ////@Cacheable(value="userMonstersForUser", key="#userId")
-  public static Map<Long, MonsterEnhancingForUser> getMonstersForUser(int userId) {
-    log.debug("retrieving user monsters being healined for userId " + userId);
+	////@Cacheable(value="userMonstersForUser", key="#userId")
+	public static Map<Long, MonsterEnhancingForUser> getMonstersForUser(int userId) {
+		log.debug("retrieving user monsters being healined for userId " + userId);
 
-    Connection conn = DBConnection.get().getConnection();
-    ResultSet rs = DBConnection.get().selectRowsByUserId(conn, userId, TABLE_NAME);
-    Map<Long, MonsterEnhancingForUser> userMonsters = convertRSToUserMonsterIdsToMonsters(rs);
-    DBConnection.get().close(rs, null, conn);
-    return userMonsters;
-  }
+		Connection conn = DBConnection.get().getConnection();
+		ResultSet rs = DBConnection.get().selectRowsByUserId(conn, userId, TABLE_NAME);
+		Map<Long, MonsterEnhancingForUser> userMonsters = convertRSToUserMonsterIdsToMonsters(rs);
+		DBConnection.get().close(rs, null, conn);
+		return userMonsters;
+	}
 
-  public static Map<Long, MonsterEnhancingForUser> getMonstersWithUserAndMonsterIds(
-  		int userId, Collection<Long> userMonsterIds) {
-  	int size = userMonsterIds.size();
-  	List<String> questions = Collections.nCopies(size, "?");
-  	String delimiter = ",";
-  	
-  	String query = "select * from " + TABLE_NAME + " where ";
-    List <Object> values = new ArrayList<Object>();
+	public static Map<Long, MonsterEnhancingForUser> getMonstersWithUserAndMonsterIds(
+			int userId, Collection<Long> userMonsterIds) {
+		int size = userMonsterIds.size();
+		List<String> questions = Collections.nCopies(size, "?");
+		String delimiter = ",";
 
-    //creating a "column in (value1,value2,...,value)" condition, prefer this over
-    //chained "or"s  e.g. (column=value1 or column=value2 or...column=value);
-    query += DBConstants.MONSTER_ENHANCING_FOR_USER__USER_ID + "=? and ";
-    values.add(userId);
-    query += DBConstants.MONSTER_ENHANCING_FOR_USER__MONSTER_FOR_USER_ID + " in (" +
-    StringUtils.getListInString(questions, delimiter) + ") and ";
-    values.add(userMonsterIds);
-    
-    Connection conn = DBConnection.get().getConnection();
-    ResultSet rs = DBConnection.get().selectDirectQueryNaive(conn, query, values);
-    Map<Long, MonsterEnhancingForUser> incompleteMonsters = convertRSToUserMonsterIdsToMonsters(rs);
-    DBConnection.get().close(rs, null, conn);
-    return incompleteMonsters;
-  }
-  
-  private static Map<Long, MonsterEnhancingForUser> convertRSToUserMonsterIdsToMonsters(ResultSet rs) {
-  	Map<Long, MonsterEnhancingForUser> monsterIdsToMonsters = new HashMap<Long, MonsterEnhancingForUser>();
-  	if (rs != null) {
-  		try {
-  			rs.last();
-  			rs.beforeFirst();
-  			while(rs.next()) {
-  				MonsterEnhancingForUser userMonster = convertRSRowToMonster(rs);
-  				if (userMonster != null) {
-  					monsterIdsToMonsters.put(userMonster.getMonsterForUserId(), userMonster);
-  				}
-  			}
-  		} catch (SQLException e) {
-  			log.error("problem with database call.", e);
-  		}
-  	}
-  	return monsterIdsToMonsters;
-  }
+		String query = "select * from " + TABLE_NAME + " where ";
+		List <Object> values = new ArrayList<Object>();
 
-  /*
-   * assumes the resultset is apprpriately set up. traverses the row it's on.
-   */
-  private static MonsterEnhancingForUser convertRSRowToMonster(ResultSet rs) throws SQLException {
-    int i = 1;
-    int userId = rs.getInt(i++);
-    long monsterForUserId = rs.getLong(i++);
-    
-    Timestamp ts = null;
-    Date expectedStartTime = null;
-    try {
-    	ts = rs.getTimestamp(i++);
-    	expectedStartTime = new Date(ts.getTime());
-    } catch (SQLException e) {
-    	//if it is null, then it most likely means this is the item being enhanced
-    	log.warn("expected start time might be null userId=" + userId +
-    			"\t monsterForUserId=" + monsterForUserId + "\t expectedStartTime", e);
-    }
-    
-//    Date queuedTime = null;
-//    try {
-//    	ts = rs.getTimestamp(i++);
-//    	queuedTime = new Date(ts.getTime());
-//    } catch (SQLException e) {
-//    	log.error("expected start time might be null userId=" + userId, e);
-//    }
-    
-    MonsterEnhancingForUser userMonster = new MonsterEnhancingForUser(userId,
-    		monsterForUserId, expectedStartTime);//, queuedTime);
-    return userMonster;
-  }
+		//creating a "column in (value1,value2,...,value)" condition, prefer this over
+		//chained "or"s  e.g. (column=value1 or column=value2 or...column=value);
+		query += DBConstants.MONSTER_ENHANCING_FOR_USER__USER_ID + "=? and ";
+		values.add(userId);
+		query += DBConstants.MONSTER_ENHANCING_FOR_USER__MONSTER_FOR_USER_ID + " in (" +
+				StringUtils.getListInString(questions, delimiter) + ") and ";
+		values.add(userMonsterIds);
+
+		Connection conn = DBConnection.get().getConnection();
+		ResultSet rs = DBConnection.get().selectDirectQueryNaive(conn, query, values);
+		Map<Long, MonsterEnhancingForUser> incompleteMonsters = convertRSToUserMonsterIdsToMonsters(rs);
+		DBConnection.get().close(rs, null, conn);
+		return incompleteMonsters;
+	}
+
+	private static Map<Long, MonsterEnhancingForUser> convertRSToUserMonsterIdsToMonsters(ResultSet rs) {
+		Map<Long, MonsterEnhancingForUser> monsterIdsToMonsters = new HashMap<Long, MonsterEnhancingForUser>();
+		if (rs != null) {
+			try {
+				rs.last();
+				rs.beforeFirst();
+				while(rs.next()) {
+					MonsterEnhancingForUser userMonster = convertRSRowToMonster(rs);
+					if (userMonster != null) {
+						monsterIdsToMonsters.put(userMonster.getMonsterForUserId(), userMonster);
+					}
+				}
+			} catch (SQLException e) {
+				log.error("problem with database call.", e);
+			}
+		}
+		return monsterIdsToMonsters;
+	}
+
+	/*
+	 * assumes the resultset is apprpriately set up. traverses the row it's on.
+	 */
+	private static MonsterEnhancingForUser convertRSRowToMonster(ResultSet rs) throws SQLException {
+		int i = 1;
+		int userId = rs.getInt(i++);
+		long monsterForUserId = rs.getLong(i++);
+
+		Timestamp ts = null;
+		Date expectedStartTime = null;
+		ts = rs.getTimestamp(i++);
+		if (!rs.wasNull()) {
+			expectedStartTime = new Date(ts.getTime());
+		}
+
+		//    Date queuedTime = null;
+		//    try {
+		//    	ts = rs.getTimestamp(i++);
+		//    	queuedTime = new Date(ts.getTime());
+		//    } catch (SQLException e) {
+		//    	log.error("expected start time might be null userId=" + userId, e);
+		//    }
+
+		MonsterEnhancingForUser userMonster = new MonsterEnhancingForUser(userId,
+				monsterForUserId, expectedStartTime);//, queuedTime);
+		return userMonster;
+	}
 
 }
