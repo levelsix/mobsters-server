c25d00646715f67127d71d10fa5bb2a8584c8c05
Arturo Wu Zhou <aweezy90@gmail.com>
2014-03-06 00:10:51 -0800

    initial gems for user is 50 not 750

--- src/main/java/com/lvl6/properties/ControllerConstants.java
+++ src/main/java/com/lvl6/properties/ControllerConstants.java
@@ -145,7 +145,7 @@ public class ControllerConstants {
 	
 	public static final int TUTORIAL__INIT_CASH = 750;
 	public static final int TUTORIAL__INIT_OIL = 750;
-	public static final int TUTORIAL__INIT_GEMS = 750;
+	public static final int TUTORIAL__INIT_GEMS = 50;
 	
 	
 	
