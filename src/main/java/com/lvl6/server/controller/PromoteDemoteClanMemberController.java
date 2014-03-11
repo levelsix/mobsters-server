6ce73288646409e9ade745b6ecd7780309da5d7b
Arturo Wu Zhou <aweezy90@gmail.com>
2014-03-11 05:10:07 -0700

    renamed victim property  to victimId

--- src/main/java/com/lvl6/server/controller/PromoteDemoteClanMemberController.java
+++ src/main/java/com/lvl6/server/controller/PromoteDemoteClanMemberController.java
@@ -49,7 +49,7 @@ import com.lvl6.utils.utilmethods.UpdateUtils;
 
     MinimumUserProto senderProto = reqProto.getSender();
     int userId = senderProto.getUserId();
-    int victimId = reqProto.getVictim();
+    int victimId = reqProto.getVictimId();
     UserClanStatus newUserClanStatus = reqProto.getUserClanStatus();
     List<Integer> userIds = new ArrayList<Integer>();
     userIds.add(userId);
