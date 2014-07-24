9641a886ff3684a7c874cc3d4873830a5a7040cf
Arturo Wu Zhou <aweezy90@gmail.com>
2014-07-24 16:11:09 -0700

    admin tool to cheat

---
+++ src/main/java/com/lvl6/events/request/DevRequestEvent.java
@@ -0,0 +1,34 @@
+package com.lvl6.events.request;
+
+import java.nio.ByteBuffer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.protobuf.ByteString;
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.lvl6.events.RequestEvent;
+import com.lvl6.proto.EventDevProto.DevRequestProto;
+
+public class DevRequestEvent extends RequestEvent {
+
+	private Logger log = LoggerFactory.getLogger(new Object() { }.getClass().getEnclosingClass());
+	
+  private DevRequestProto devRequestProto;
+  
+  /**
+   * read the event from the given ByteBuffer to populate this event
+   */
+  public void read(ByteBuffer buff) {
+    try {
+      devRequestProto = DevRequestProto.parseFrom(ByteString.copyFrom(buff));
+      playerId = devRequestProto.getSender().getUserId();
+    } catch (InvalidProtocolBufferException e) {
+      log.error("enable apns request exception", e);
+    }
+  }
+
+  public DevRequestProto getDevRequestProto() {
+    return devRequestProto;
+  }
+}
---
+++ src/main/java/com/lvl6/events/response/DevResponseEvent.java
@@ -0,0 +1,30 @@
+package com.lvl6.events.response;
+
+import java.nio.ByteBuffer;
+
+import com.google.protobuf.ByteString;
+import com.lvl6.events.NormalResponseEvent;
+import com.lvl6.proto.EventDevProto.DevResponseProto;
+import com.lvl6.proto.ProtocolsProto.EventProtocolResponse;
+
+public class DevResponseEvent extends NormalResponseEvent {
+
+  private DevResponseProto devResponseProto;
+  
+  public DevResponseEvent(int playerId){
+    super(playerId);
+    eventType = EventProtocolResponse.S_DEV_EVENT;
+  }
+  
+  @Override
+  public int write(ByteBuffer bb) {
+    ByteString b = devResponseProto.toByteString();
+    b.copyTo(bb);
+    return b.size();
+  }
+
+  public void setDevResponseProto(DevResponseProto devResponseProto) {
+    this.devResponseProto = devResponseProto;
+  }
+
+}
