b6ce089c8b498ee6a6e491b046156a20bcd55b65
Ashwin Kamath <ashwin@lvl6.com>
2014-11-13 22:14:35 -0800

    updated delete utils

--- src/main/java/com/lvl6/utils/utilmethods/DeleteUtils.java
+++ src/main/java/com/lvl6/utils/utilmethods/DeleteUtils.java
@@ -52,7 +52,7 @@ public class DeleteUtils implements DeleteUtil {
   /*@Caching(evict= {
       //@CacheEvict(value="structIdsToUserStructsForUser", allEntries=true),
       //@CacheEvict(value="specificUserStruct", key="#userStructId")})*/
-  public boolean deleteUserStruct(int userStructId) {
+  public boolean deleteUserStruct(String userStructId) {
     Map <String, Object> conditionParams = new HashMap<String, Object>();
     conditionParams.put(DBConstants.STRUCTURE_FOR_USER__ID, userStructId);
     int numDeleted = DBConnection.get().deleteRows(DBConstants.TABLE_STRUCTURE_FOR_USER, conditionParams, "and");
@@ -62,7 +62,7 @@ public class DeleteUtils implements DeleteUtil {
     return false;
   }
   
-  public boolean deleteUserClanDataRelatedToClanId(int clanId, int numRowsToDelete) {
+  public boolean deleteUserClanDataRelatedToClanId(String clanId, int numRowsToDelete) {
     Map <String, Object> conditionParams = new HashMap<String, Object>();
     conditionParams.put(DBConstants.CLAN_FOR_USER__CLAN_ID, clanId);
     int numDeleted = DBConnection.get().deleteRows(DBConstants.TABLE_CLAN_FOR_USER, conditionParams, "and");
@@ -72,7 +72,7 @@ public class DeleteUtils implements DeleteUtil {
     return false;
   }
   
-  public void deleteUserClansForUserExceptSpecificClan(int userId, int clanId) {
+  public void deleteUserClansForUserExceptSpecificClan(String userId, String clanId) {
     String query = "delete from " + DBConstants.TABLE_CLAN_FOR_USER + " where " + DBConstants.CLAN_FOR_USER__USER_ID + "=? and " +
         DBConstants.CLAN_FOR_USER__CLAN_ID + "!=?";
     
@@ -84,7 +84,7 @@ public class DeleteUtils implements DeleteUtil {
   }
   
   
-  public boolean deleteUserClan(int userId, int clanId) {
+  public boolean deleteUserClan(String userId, String clanId) {
     Map <String, Object> conditionParams = new HashMap<String, Object>();
     conditionParams.put(DBConstants.CLAN_FOR_USER__CLAN_ID, clanId);
     conditionParams.put(DBConstants.CLAN_FOR_USER__USER_ID, userId);
@@ -98,7 +98,7 @@ public class DeleteUtils implements DeleteUtil {
 
   
   //@CacheEvict(value="clanById", key="#clanId")
-  public boolean deleteClanWithClanId(int clanId) {
+  public boolean deleteClanWithClanId(String clanId) {
     Map <String, Object> conditionParams = new HashMap<String, Object>();
     conditionParams.put(DBConstants.CLANS__ID, clanId);
     int numDeleted = DBConnection.get().deleteRows(DBConstants.TABLE_CLANS, conditionParams, "and");
@@ -108,7 +108,7 @@ public class DeleteUtils implements DeleteUtil {
     return false;
   }
 
-  public int deleteAllUserQuestsForUser(int userId) {
+  public int deleteAllUserQuestsForUser(String userId) {
     String tableName = DBConstants.TABLE_QUEST_FOR_USER;
     String condDelim = "and";
     Map <String, Object> conditionParams = new HashMap<String, Object>();
@@ -119,7 +119,7 @@ public class DeleteUtils implements DeleteUtil {
   }
   
   @Override
-  public int deleteTaskForUserOngoingWithTaskForUserId(long taskForUserId) {
+  public int deleteTaskForUserOngoingWithTaskForUserId(String taskForUserId) {
     String tableName = DBConstants.TABLE_TASK_FOR_USER_ONGOING;
     String condDelim = "and";
     Map <String, Object> conditionParams = new HashMap<String, Object>();
@@ -131,7 +131,7 @@ public class DeleteUtils implements DeleteUtil {
   }
   
   @Override
-  public int deleteTaskStagesForUserWithIds(List<Long> taskStageForUserIds) {
+  public int deleteTaskStagesForUserWithIds(List<String> taskStageForUserIds) {
   	String tableName = DBConstants.TABLE_TASK_STAGE_FOR_USER;
   	int size = taskStageForUserIds.size();
     List<String> questions = Collections.nCopies(size, "?");
@@ -145,7 +145,7 @@ public class DeleteUtils implements DeleteUtil {
   }
   
   @Override
-  public int deleteMonsterHealingForUser(int userId, List<Long> userMonsterIds) {
+  public int deleteMonsterHealingForUser(String userId, List<String> userMonsterIds) {
   	String tableName = DBConstants.TABLE_MONSTER_HEALING_FOR_USER;
   	int size = userMonsterIds.size();
   	
@@ -168,7 +168,7 @@ public class DeleteUtils implements DeleteUtil {
   }
   
   @Override
-  public int deleteMonsterEnhancingForUser(int userId, List<Long> userMonsterIds) {
+  public int deleteMonsterEnhancingForUser(String userId, List<String> userMonsterIds) {
   	String tableName = DBConstants.TABLE_MONSTER_ENHANCING_FOR_USER;
   	int size = userMonsterIds.size();
   	
@@ -192,7 +192,7 @@ public class DeleteUtils implements DeleteUtil {
 
   ////@CacheEvict(value ="specificUserEquip", key="#userEquipId")
   @Override
-  public int deleteMonsterForUser(long userMonsterId) {
+  public int deleteMonsterForUser(String userMonsterId) {
     Map <String, Object> conditionParams = new HashMap<String, Object>();
     conditionParams.put(DBConstants.MONSTER_FOR_USER__ID, userMonsterId);
 
@@ -201,7 +201,7 @@ public class DeleteUtils implements DeleteUtil {
   }
   
   @Override
-  public int deleteMonstersForUser(List<Long> userMonsterIds) {
+  public int deleteMonstersForUser(List<String> userMonsterIds) {
     String tableName = DBConstants.TABLE_MONSTER_FOR_USER;
     int size = userMonsterIds.size();
     List<String> questions = Collections.nCopies(size, "?");
@@ -220,7 +220,7 @@ public class DeleteUtils implements DeleteUtil {
   }
 
 	@Override
-	public int deleteUserFacebookInvitesForSlots(List<Integer> idsOfInvites) {
+	public int deleteUserFacebookInvitesForSlots(List<String> idsOfInvites) {
 		String tableName = DBConstants.TABLE_USER_FACEBOOK_INVITE_FOR_SLOT;
     int size = idsOfInvites.size();
     List<String> questions = Collections.nCopies(size, "?");
@@ -241,7 +241,7 @@ public class DeleteUtils implements DeleteUtil {
 	}
 
 	@Override
-	public int deleteUnredeemedUserFacebookInvitesForUser(int userId) {
+	public int deleteUnredeemedUserFacebookInvitesForUser(String userId) {
 		String tableName = DBConstants.TABLE_USER_FACEBOOK_INVITE_FOR_SLOT;
 		
 		StringBuilder querySb = new StringBuilder();
@@ -257,15 +257,15 @@ public class DeleteUtils implements DeleteUtil {
 		querySb.append(" IS NULL");
 		
 		String query = querySb.toString();
-		List<Integer> values = new ArrayList<Integer>();
+		List<String> values = new ArrayList<String>();
 		values.add(userId);
 		int numDeleted = DBConnection.get().deleteDirectQueryNaive(query, values);
 		return numDeleted;
 	}
 
 	@Override
-	public int deleteMonsterEvolvingForUser(long catalystUserMonsterId,
-			long userMonsterIdOne, long userMonsterIdTwo, int userId) {
+	public int deleteMonsterEvolvingForUser(String catalystUserMonsterId,
+	    String userMonsterIdOne, String userMonsterIdTwo, String userId) {
 		String tableName = DBConstants.TABLE_MONSTER_EVOLVING_FOR_USER;
 		
 		Map <String, Object> conditionParams = new HashMap<String, Object>();
@@ -282,7 +282,7 @@ public class DeleteUtils implements DeleteUtil {
 	}
 	
 	@Override
-	public int deleteEventPersistentForUser(int userId, int eventId) {
+	public int deleteEventPersistentForUser(String userId, int eventId) {
 		String tableName = DBConstants.TABLE_EVENT_PERSISTENT_FOR_USER;
 		
 		Map <String, Object> conditionParams = new HashMap<String, Object>();
@@ -294,7 +294,7 @@ public class DeleteUtils implements DeleteUtil {
 	}
 	
 	@Override
-	public int deletePvpBattleForUser(int attackerId) {
+	public int deletePvpBattleForUser(String attackerId) {
 		String tableName = DBConstants.TABLE_PVP_BATTLE_FOR_USER;
 		
 		Map <String, Object> conditionParams = new HashMap<String, Object>();
@@ -305,7 +305,7 @@ public class DeleteUtils implements DeleteUtil {
 	}
 	
 	@Override
-	public int deleteClanEventPersistentForClan(int clanId) {
+	public int deleteClanEventPersistentForClan(String clanId) {
 		String tableName = DBConstants.TABLE_CLAN_EVENT_PERSISTENT_FOR_CLAN;
 		
 		Map <String, Object> conditionParams = new HashMap<String, Object>();
@@ -316,7 +316,7 @@ public class DeleteUtils implements DeleteUtil {
 	}
 	
 	@Override
-	public int deleteClanEventPersistentForUsers(List<Integer> userIdList) {
+	public int deleteClanEventPersistentForUsers(List<String> userIdList) {
 		String tableName = DBConstants.TABLE_CLAN_EVENT_PERSISTENT_FOR_USER;
 		int size = userIdList.size();
     List<String> questions = Collections.nCopies(size, "?");
@@ -337,7 +337,7 @@ public class DeleteUtils implements DeleteUtil {
 	}
 	
 	@Override
-	public int deleteObstacleForUser(int userObstacleId) {
+	public int deleteObstacleForUser(String userObstacleId) {
 		String tableName = DBConstants.TABLE_OBSTACLE_FOR_USER;
 
 		Map<String, Object> conditionParams = new HashMap<String, Object>();
@@ -348,7 +348,7 @@ public class DeleteUtils implements DeleteUtil {
 	}
 	
 	@Override
-	public int deleteMiniJobForUser(long userMiniJobId) {
+	public int deleteMiniJobForUser(String userMiniJobId) {
 		String tableName = DBConstants.TABLE_MINI_JOB_FOR_USER;
 
 		Map<String, Object> conditionParams = new HashMap<String, Object>();
@@ -359,7 +359,7 @@ public class DeleteUtils implements DeleteUtil {
 	}
 
 	@Override
-	public int deleteClanHelp( int userId, List<Long> clanHelpIdList )
+	public int deleteClanHelp( String userId, List<String> clanHelpIdList )
 	{
 		String tableName = DBConstants.TABLE_CLAN_HELP;
 		
@@ -382,7 +382,7 @@ public class DeleteUtils implements DeleteUtil {
 	}
 	
 	@Override
-	public int deleteClanInvite(int userId, List<Integer> clanInviteIdList) {
+	public int deleteClanInvite(String userId, List<String> clanInviteIdList) {
 		String tableName = DBConstants.TABLE_CLAN_INVITE;
 
 		String query = null;
