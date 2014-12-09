1ed43100dfdc2a664b509413d45dc259b0e577c8
bluestix <kelly@dekayd.com>
2014-12-09 06:42:00 -0800

    Adding clan search and start of SNS APNS

--- src/main/java/com/lvl6/clansearch/ClanSearch.java
+++ src/main/java/com/lvl6/clansearch/ClanSearch.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.hazelcast.core.HazelcastInstance;
@@ -19,7 +20,7 @@ public class ClanSearch {
 	private static Long DAY = 24*60*60*1000l;
 	private static Integer idealNumberOfMembers = 80;
 	
-	
+	@Autowired
 	protected HazelcastInstance hz;
 	protected DistributedZSet rankedClans;
 	
