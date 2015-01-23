90d8b6dbc21be6f2411392a5c743d36319240523
Arturo Wu Zhou <aweezy90@gmail.com>
2015-01-22 17:59:10 -0800

    changing user starting monster

--- src/main/java/com/lvl6/properties/ControllerConstants.java
+++ src/main/java/com/lvl6/properties/ControllerConstants.java
@@ -212,7 +212,7 @@ public class ControllerConstants {
 	//the monster id that is used in tutorial
 	public static final int TUTORIAL__STARTING_MONSTER_ID = 2011;
 	//the monster id the user gets to use after tutorial
-	public static final int TUTORIAL__USER_STARTING_MONSTER_ID = 37;
+	public static final int TUTORIAL__USER_STARTING_MONSTER_ID = 2011;
 	public static final int TUTORIAL__GUIDE_MONSTER_ID = 2012;
 	public static final int TUTORIAL__ENEMY_MONSTER_ID_ONE = 2010;
 	public static final int TUTORIAL__ENEMY_MONSTER_ID_TWO = 1100;
