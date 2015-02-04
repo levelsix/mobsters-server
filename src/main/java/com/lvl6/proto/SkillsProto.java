8ba3b40721af91d854b0dba0767765286f2e37c0
Rob Giusti <rockenstein.rock@gmail.com>
2015-02-04 09:17:16 -0800

    Added skill enum values for Left Hook, Curse, and Insurance

--- src/main/java/com/lvl6/proto/SkillsProto.java
+++ src/main/java/com/lvl6/proto/SkillsProto.java
@@ -141,6 +141,18 @@ public final class SkillsProto {
      * <code>RIGHT_HOOK = 30;</code>
      */
     RIGHT_HOOK(29, 30),
+    /**
+     * <code>CURSE = 31;</code>
+     */
+    CURSE(30, 31),
+    /**
+     * <code>LEFT_HOOK = 32;</code>
+     */
+    LEFT_HOOK(31, 32),
+    /**
+     * <code>INSURANCE = 33;</code>
+     */
+    INSURANCE(32, 33),
     ;
 
     /**
@@ -271,6 +283,18 @@ public final class SkillsProto {
      * <code>RIGHT_HOOK = 30;</code>
      */
     public static final int RIGHT_HOOK_VALUE = 30;
+    /**
+     * <code>CURSE = 31;</code>
+     */
+    public static final int CURSE_VALUE = 31;
+    /**
+     * <code>LEFT_HOOK = 32;</code>
+     */
+    public static final int LEFT_HOOK_VALUE = 32;
+    /**
+     * <code>INSURANCE = 33;</code>
+     */
+    public static final int INSURANCE_VALUE = 33;
 
 
     public final int getNumber() { return value; }
@@ -307,6 +331,9 @@ public final class SkillsProto {
         case 28: return HELL_FIRE;
         case 29: return ENERGIZE;
         case 30: return RIGHT_HOOK;
+        case 31: return CURSE;
+        case 32: return LEFT_HOOK;
+        case 33: return INSURANCE;
         default: return null;
       }
     }
@@ -3010,7 +3037,7 @@ public final class SkillsProto {
       "oto\022\014\n\004desc\030\t \001(\t\022\025\n\rimgNamePrefix\030\014 \001(\t" +
       "\"O\n\022SkillPropertyProto\022\027\n\017skillPropertyI" +
       "d\030\001 \001(\005\022\014\n\004name\030\002 \001(\t\022\022\n\nskillValue\030\003 \001(",
-      "\002*\324\003\n\tSkillType\022\014\n\010NO_SKILL\020\001\022\r\n\tCAKE_DR" +
+      "\002*\375\003\n\tSkillType\022\014\n\010NO_SKILL\020\001\022\r\n\tCAKE_DR" +
       "OP\020\002\022\t\n\005JELLY\020\003\022\020\n\014QUICK_ATTACK\020\004\022\t\n\005BOM" +
       "BS\020\005\022\n\n\006SHIELD\020\006\022\n\n\006POISON\020\007\022\r\n\tROID_RAG" +
       "E\020\010\022\014\n\010MOMENTUM\020\t\022\016\n\nTHICK_SKIN\020\n\022\022\n\016CRI" +
@@ -3021,10 +3048,11 @@ public final class SkillsProto {
       "POISON_POWDER\020\025\022\n\n\006SKEWER\020\026\022\014\n\010KNOCKOUT\020" +
       "\027\022\021\n\rSHALLOW_GRAVE\020\030\022\017\n\013HAMMER_TIME\020\031\022\016\n",
       "\nBLOOD_RAGE\020\032\022\014\n\010TAKE_AIM\020\033\022\r\n\tHELL_FIRE" +
-      "\020\034\022\014\n\010ENERGIZE\020\035\022\016\n\nRIGHT_HOOK\020\036*J\n\023Skil" +
-      "lActivationType\022\022\n\016USER_ACTIVATED\020\001\022\022\n\016A" +
-      "UTO_ACTIVATED\020\002\022\013\n\007PASSIVE\020\003B\rB\013SkillsPr" +
-      "oto"
+      "\020\034\022\014\n\010ENERGIZE\020\035\022\016\n\nRIGHT_HOOK\020\036\022\t\n\005CURS" +
+      "E\020\037\022\r\n\tLEFT_HOOK\020 \022\r\n\tINSURANCE\020!*J\n\023Ski" +
+      "llActivationType\022\022\n\016USER_ACTIVATED\020\001\022\022\n\016" +
+      "AUTO_ACTIVATED\020\002\022\013\n\007PASSIVE\020\003B\rB\013SkillsP" +
+      "roto"
     };
     com.google.protobuf.Descriptors.FileDescriptor.InternalDescriptorAssigner assigner =
         new com.google.protobuf.Descriptors.FileDescriptor.    InternalDescriptorAssigner() {
