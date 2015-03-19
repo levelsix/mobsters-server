cb19dc583722d9a36847fe0bc68cba0d156529ed
Rob Giusti <rockenstein.rock@gmail.com>
2015-03-19 15:46:22 -0700

    protos

--- src/main/java/com/lvl6/proto/SkillsProto.java
+++ src/main/java/com/lvl6/proto/SkillsProto.java
@@ -161,6 +161,10 @@ public final class SkillsProto {
      * <code>POISON_FIRE = 35;</code>
      */
     POISON_FIRE(34, 35),
+    /**
+     * <code>CHILL = 36;</code>
+     */
+    CHILL(35, 36),
     ;
 
     /**
@@ -311,6 +315,10 @@ public final class SkillsProto {
      * <code>POISON_FIRE = 35;</code>
      */
     public static final int POISON_FIRE_VALUE = 35;
+    /**
+     * <code>CHILL = 36;</code>
+     */
+    public static final int CHILL_VALUE = 36;
 
 
     public final int getNumber() { return value; }
@@ -352,6 +360,7 @@ public final class SkillsProto {
         case 33: return FLAME_BREAK;
         case 34: return POISON_SKEWER;
         case 35: return POISON_FIRE;
+        case 36: return CHILL;
         default: return null;
       }
     }
@@ -6208,7 +6217,7 @@ public final class SkillsProto {
       "ionType:\017BELOW_CHARACTER\022\027\n\017pfxPixelOffs" +
       "etX\030\r \001(\005\022\027\n\017pfxPixelOffsetY\030\016 \001(\005\022K\n\tbl" +
       "endMode\030\017 \001(\0162#.com.lvl6.proto.SideEffec" +
-      "tBlendMode:\023NORMAL_FULL_OPACITY*\243\004\n\tSkil" +
+      "tBlendMode:\023NORMAL_FULL_OPACITY*\256\004\n\tSkil" +
       "lType\022\014\n\010NO_SKILL\020\001\022\r\n\tCAKE_DROP\020\002\022\t\n\005JE" +
       "LLY\020\003\022\020\n\014QUICK_ATTACK\020\004\022\t\n\005BOMBS\020\005\022\n\n\006SH" +
       "IELD\020\006\022\n\n\006POISON\020\007\022\r\n\tROID_RAGE\020\010\022\014\n\010MOM" +
@@ -6222,24 +6231,24 @@ public final class SkillsProto {
       "E\020\032\022\014\n\010TAKE_AIM\020\033\022\r\n\tHELL_FIRE\020\034\022\014\n\010ENER" +
       "GIZE\020\035\022\016\n\nRIGHT_HOOK\020\036\022\t\n\005CURSE\020\037\022\r\n\tINS" +
       "URANCE\020 \022\017\n\013FLAME_BREAK\020!\022\021\n\rPOISON_SKEW" +
-      "ER\020\"\022\017\n\013POISON_FIRE\020#*J\n\023SkillActivation",
-      "Type\022\022\n\016USER_ACTIVATED\020\001\022\022\n\016AUTO_ACTIVAT" +
-      "ED\020\002\022\013\n\007PASSIVE\020\003*\231\003\n\016SideEffectType\022\022\n\016" +
-      "NO_SIDE_EFFECT\020\001\022\017\n\013NERF_POISON\020\002\022\016\n\nNER" +
-      "F_CURSE\020\003\022\r\n\tNERF_STUN\020\004\022\022\n\016NERF_CONFUSI" +
-      "ON\020\005\022\027\n\023NERF_BLINDING_LIGHT\020\006\022\023\n\017NERF_BL" +
-      "OOD_RAGE\020\007\022\021\n\rBUFF_MOMENTUM\020\010\022\023\n\017BUFF_TH" +
-      "ICK_SKIN\020\t\022\026\n\022BUFF_DRUNKEN_BRAWL\020\n\022\030\n\024BU" +
-      "FF_CRITICAL_STRIKE\020\013\022\016\n\nBUFF_EVADE\020\014\022\025\n\021" +
-      "BUFF_STATIC_FIELD\020\r\022\027\n\023BUFF_COUNTER_STRI" +
-      "KE\020\016\022\022\n\016BUFF_ROID_RAGE\020\017\022\026\n\022BUFF_SHALLOW",
-      "_GRAVE\020\020\022\021\n\rBUFF_ENERGIZE\020\021\022\022\n\016BUFF_INSU" +
-      "RANCE\020\022\022\024\n\020BUFF_HAMMER_TIME\020\023*7\n\023SideEff" +
-      "ectTraitType\022\014\n\010NO_TRAIT\020\001\022\010\n\004BUFF\020\002\022\010\n\004" +
-      "NERF\020\003*B\n\026SideEffectPositionType\022\023\n\017BELO" +
-      "W_CHARACTER\020\001\022\023\n\017ABOVE_CHARACTER\020\002*.\n\023Si" +
-      "deEffectBlendMode\022\027\n\023NORMAL_FULL_OPACITY" +
-      "\020\001B\rB\013SkillsProto"
+      "ER\020\"\022\017\n\013POISON_FIRE\020#\022\t\n\005CHILL\020$*J\n\023Skil",
+      "lActivationType\022\022\n\016USER_ACTIVATED\020\001\022\022\n\016A" +
+      "UTO_ACTIVATED\020\002\022\013\n\007PASSIVE\020\003*\231\003\n\016SideEff" +
+      "ectType\022\022\n\016NO_SIDE_EFFECT\020\001\022\017\n\013NERF_POIS" +
+      "ON\020\002\022\016\n\nNERF_CURSE\020\003\022\r\n\tNERF_STUN\020\004\022\022\n\016N" +
+      "ERF_CONFUSION\020\005\022\027\n\023NERF_BLINDING_LIGHT\020\006" +
+      "\022\023\n\017NERF_BLOOD_RAGE\020\007\022\021\n\rBUFF_MOMENTUM\020\010" +
+      "\022\023\n\017BUFF_THICK_SKIN\020\t\022\026\n\022BUFF_DRUNKEN_BR" +
+      "AWL\020\n\022\030\n\024BUFF_CRITICAL_STRIKE\020\013\022\016\n\nBUFF_" +
+      "EVADE\020\014\022\025\n\021BUFF_STATIC_FIELD\020\r\022\027\n\023BUFF_C" +
+      "OUNTER_STRIKE\020\016\022\022\n\016BUFF_ROID_RAGE\020\017\022\026\n\022B",
+      "UFF_SHALLOW_GRAVE\020\020\022\021\n\rBUFF_ENERGIZE\020\021\022\022" +
+      "\n\016BUFF_INSURANCE\020\022\022\024\n\020BUFF_HAMMER_TIME\020\023" +
+      "*7\n\023SideEffectTraitType\022\014\n\010NO_TRAIT\020\001\022\010\n" +
+      "\004BUFF\020\002\022\010\n\004NERF\020\003*B\n\026SideEffectPositionT" +
+      "ype\022\023\n\017BELOW_CHARACTER\020\001\022\023\n\017ABOVE_CHARAC" +
+      "TER\020\002*.\n\023SideEffectBlendMode\022\027\n\023NORMAL_F" +
+      "ULL_OPACITY\020\001B\rB\013SkillsProto"
     };
     com.google.protobuf.Descriptors.FileDescriptor.InternalDescriptorAssigner assigner =
         new com.google.protobuf.Descriptors.FileDescriptor.    InternalDescriptorAssigner() {
