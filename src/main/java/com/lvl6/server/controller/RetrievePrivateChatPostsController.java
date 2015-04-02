8ab75e80bc10a99c019f6d996fad7e5b48d1ad3b
Byron Tang <byronyugontang@gmail.cpm>
2015-04-02 11:18:29 -0700

    cleaning up cherry pick commits

--- src/main/java/com/lvl6/server/controller/RetrievePrivateChatPostsController.java
+++ src/main/java/com/lvl6/server/controller/RetrievePrivateChatPostsController.java
@@ -132,6 +132,11 @@ public class RetrievePrivateChatPostsController extends EventController {
 						//for not hitting the db for every private chat post
 						Map<String, MinimumUserProtoWithLevel> userIdsToMups = generateUserIdsToMupsWithLevel(
 								usersByIds, userId, senderProto, otherUserId);
+						
+						List<String> chatIds = new ArrayList<String>();
+						for(PrivateChatPost pcp : recentPrivateChatPosts) {
+							chatIds.add(pcp.getId());
+						}
 
 						List<String> chatIds = new ArrayList<String>();
 						for(PrivateChatPost pcp : recentPrivateChatPosts) {
