c4f93385864c149090f8f91ca5c0dbe8959544d8
Byron Tang <byronyugontang@gmail.cpm>
2015-04-01 20:14:30 -0700

    more logs

--- src/main/java/com/lvl6/server/controller/SendGroupChatController.java
+++ src/main/java/com/lvl6/server/controller/SendGroupChatController.java
@@ -25,11 +25,9 @@ import com.lvl6.events.response.ReceivedGroupChatResponseEvent;
 import com.lvl6.events.response.SendGroupChatResponseEvent;
 import com.lvl6.events.response.UpdateClientUserResponseEvent;
 import com.lvl6.info.Clan;
-import com.lvl6.info.TranslationSettingsForUser;
 import com.lvl6.info.User;
 import com.lvl6.misc.MiscMethods;
 import com.lvl6.properties.ControllerConstants;
-import com.lvl6.proto.ChatProto.ChatType;
 import com.lvl6.proto.ChatProto.GroupChatMessageProto;
 import com.lvl6.proto.ChatProto.GroupChatScope;
 import com.lvl6.proto.ChatProto.TranslateLanguages;
@@ -51,7 +49,6 @@ import com.lvl6.server.EventWriter;
 import com.lvl6.server.Locker;
 import com.lvl6.utils.CreateInfoProtoUtils;
 import com.lvl6.utils.utilmethods.InsertUtils;
-import com.memetix.mst.language.Language;
 
 @Component
 @DependsOn("gameServer")
@@ -183,12 +180,14 @@ public class SendGroupChatController extends EventController {
 				
 				chatProto.setMessage(gcmp);	
 
-				
+				log.info("chatproto: {}", chatProto);
+				ReceivedGroupChatResponseProto rgcr = chatProto.build();
+				log.info("chatproto: {}", rgcr);
 				
 				sendChatMessage(userId, chatProto, event.getTag(),
 						scope == GroupChatScope.CLAN, user.getClanId(),
 						user.isAdmin(), timeOfPost.getTime(), user.getLevel(),
-						censoredChatMessage);
+						censoredChatMessage, rgcr);
 				
 				// send messages in background so sending player can unlock
 				/*
@@ -218,10 +217,10 @@ public class SendGroupChatController extends EventController {
 	protected void sendChatMessage(String senderId,
 			ReceivedGroupChatResponseProto.Builder chatProto, int tag,
 			boolean isForClan, String clanId, boolean isAdmin, long time,
-			int level, String censoredChatMessage) {
+			int level, String censoredChatMessage, ReceivedGroupChatResponseProto rgcr) {
 		ReceivedGroupChatResponseEvent ce = new ReceivedGroupChatResponseEvent(
 				senderId);
-		ce.setReceivedGroupChatResponseProto(chatProto.build());
+		ce.setReceivedGroupChatResponseProto(rgcr);
 		if (isForClan) {
 			log.info("Sending event to clan " + clanId);
 			eventWriter.handleClanEvent(ce, clanId);
