22fc569fd98672fd1db53eb6adecfda0f71a5f26
Byron Tang <byronyugontang@gmail.cpm>
2015-04-05 17:34:17 -0700

    new reloadrarechange and secretgiftutil files

---
+++ src/main/java/com/lvl6/misc/ReloadAllRareChangeStaticData.java
@@ -0,0 +1,316 @@
+package com.lvl6.misc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.stereotype.Component;
+
+import com.lvl6.retrieveutils.rarechange.AchievementRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.BannedUserRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.BattleItemRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.BoardObstacleRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.BoardPropertyRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.BoardRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.BoosterDisplayItemRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.BoosterItemRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.BoosterPackRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.ClanEventPersistentRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.ClanIconRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.ClanRaidRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.ClanRaidStageMonsterRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.ClanRaidStageRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.ClanRaidStageRewardRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.EventPersistentRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.FileDownloadRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.GoldSaleRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.ItemRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.MiniJobRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.MonsterBattleDialogueRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.MonsterLevelInfoRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.MonsterRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.ObstacleRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.PrerequisiteRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.ProfanityRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.PvpLeagueRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.QuestJobMonsterItemRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.QuestJobRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.QuestRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.ResearchPropertyRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.ResearchRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.SkillPropertyRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.SkillRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.SkillSideEffectRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.StartupStuffRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.StaticUserLevelInfoRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.StructureBattleItemFactoryRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.StructureClanHouseRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.StructureEvoChamberRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.StructureHospitalRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.StructureLabRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.StructureMiniJobRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.StructureMoneyTreeRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.StructurePvpBoardRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.StructureResearchHouseRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.StructureResidenceRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.StructureResourceGeneratorRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.StructureResourceStorageRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.StructureRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.StructureTeamCenterRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.StructureTownHallRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.TaskMapElementRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.TaskRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.TaskStageMonsterRetrieveUtils;
+import com.lvl6.retrieveutils.rarechange.TaskStageRetrieveUtils;
+
+@Component
+@DependsOn("gameServer")
+public class ReloadAllRareChangeStaticData {
+
+	@Autowired
+	protected AchievementRetrieveUtils achievementRetrieveUtils;
+	
+	@Autowired
+	protected BannedUserRetrieveUtils bannedUserRetrieveUtils;
+	
+	@Autowired
+	protected BattleItemRetrieveUtils battleItemRetrieveUtils;
+	
+	@Autowired
+	protected BoardObstacleRetrieveUtils boardObstacleRetrieveUtils;
+	
+	@Autowired
+	protected BoardRetrieveUtils boardRetrieveUtils;
+
+	@Autowired
+	protected BoardPropertyRetrieveUtils boardPropertyRetrieveUtils;
+	
+	@Autowired
+	protected BoosterDisplayItemRetrieveUtils boosterDisplayItemRetrieveUtils;
+	
+	@Autowired
+	protected BoosterItemRetrieveUtils boosterItemRetrieveUtils;
+	
+	@Autowired
+	protected BoosterPackRetrieveUtils boosterPackRetrieveUtils;
+	
+	@Autowired
+	protected ClanIconRetrieveUtils clanIconRetrieveUtils;
+	
+	@Autowired
+	protected ClanEventPersistentRetrieveUtils clanEventPersistentRetrieveUtils;
+	
+	@Autowired
+	protected ClanRaidRetrieveUtils clanRaidRetrieveUtils;
+	
+	@Autowired
+	protected ClanRaidStageRetrieveUtils clanRaidStageRetrieveUtils;
+	
+	@Autowired
+	protected ClanRaidStageMonsterRetrieveUtils clanRaidStageMonsterRetrieveUtils;
+	
+	@Autowired
+	protected ClanRaidStageRewardRetrieveUtils clanRaidStageRewardRetrieveUtils;
+	
+	@Autowired
+	protected EventPersistentRetrieveUtils eventPersistentRetrieveUtils;
+	
+	@Autowired
+	protected FileDownloadRetrieveUtils fileDownloadRetrieveUtils;
+	
+	@Autowired
+	protected GoldSaleRetrieveUtils goldSaleRetrieveUtils;
+	
+	@Autowired
+	protected ItemRetrieveUtils itemRetrieveUtils;
+	
+	@Autowired
+	protected MiniJobRetrieveUtils miniJobRetrieveUtils;
+	
+	@Autowired
+	protected MonsterBattleDialogueRetrieveUtils monsterBattleDialogueRetrieveUtils;
+	
+	@Autowired
+	protected MonsterLevelInfoRetrieveUtils monsterLevelInfoRetrieveUtils;
+	
+	@Autowired
+	protected MonsterRetrieveUtils monsterRetrieveUtils;
+	
+	@Autowired
+	protected ObstacleRetrieveUtils obstacleRetrieveUtils;
+	
+	@Autowired
+	protected PrerequisiteRetrieveUtils prerequisiteRetrieveUtils;
+	
+	@Autowired
+	protected ProfanityRetrieveUtils profanityRetrieveUtils;
+	
+	@Autowired
+	protected PvpLeagueRetrieveUtils pvpLeagueRetrieveUtils;
+	
+	@Autowired
+	protected QuestJobRetrieveUtils questJobRetrieveUtils;
+	
+	@Autowired
+	protected QuestJobMonsterItemRetrieveUtils questJobMonsterItemRetrieveUtils;
+	
+	@Autowired
+	protected QuestRetrieveUtils questRetrieveUtils;
+	
+	@Autowired
+	protected ResearchRetrieveUtils researchRetrieveUtils;
+	
+	@Autowired
+	protected ResearchPropertyRetrieveUtils researchPropertyRetrieveUtils;
+	
+	@Autowired
+	protected SkillRetrieveUtils skillRetrieveUtils;
+	
+	@Autowired
+	protected SkillPropertyRetrieveUtils skillPropertyRetrieveUtils;
+	
+	@Autowired
+	protected SkillSideEffectRetrieveUtils skillSideEffectRetrieveUtils;
+	
+	@Autowired
+	protected StartupStuffRetrieveUtils startupStuffRetrieveUtils;
+ 
+	@Autowired
+	protected StaticUserLevelInfoRetrieveUtils staticUserLevelInfoRetrieveUtils;
+	
+	@Autowired
+	protected StructureBattleItemFactoryRetrieveUtils structureBattleItemFactoryRetrieveUtils;
+	
+	@Autowired
+	protected StructureClanHouseRetrieveUtils structureClanHouseRetrieveUtils;
+	
+	@Autowired
+	protected StructureEvoChamberRetrieveUtils structureEvoChamberRetrieveUtils;
+	
+	@Autowired
+	protected StructureHospitalRetrieveUtils structureHospitalRetrieveUtils;
+	
+	@Autowired
+	protected StructureLabRetrieveUtils structureLabRetrieveUtils;
+	
+	@Autowired
+	protected StructureMiniJobRetrieveUtils structureMiniJobRetrieveUtils;
+	
+	@Autowired
+	protected StructureMoneyTreeRetrieveUtils structureMoneyTreeRetrieveUtils;
+	
+	@Autowired
+	protected StructurePvpBoardRetrieveUtils structurePvpBoardRetrieveUtils;
+	
+	@Autowired
+	protected StructureResearchHouseRetrieveUtils structureResearchHouseRetrieveUtils;
+	
+	@Autowired
+	protected StructureResidenceRetrieveUtils structureResidenceRetrieveUtils;
+	
+	@Autowired
+	protected StructureResourceGeneratorRetrieveUtils structureResourceGeneratorRetrieveUtils;
+	
+	@Autowired
+	protected StructureResourceStorageRetrieveUtils structureResourceStorageRetrieveUtils;
+	
+	@Autowired
+	protected StructureRetrieveUtils structureRetrieveUtils;
+	
+	@Autowired
+	protected StructureTeamCenterRetrieveUtils structureTeamCenterRetrieveUtils;
+	
+	@Autowired
+	protected StructureTownHallRetrieveUtils structureTownHallRetrieveUtils;
+	
+	@Autowired
+	protected TaskMapElementRetrieveUtils taskMapElementRetrieveUtils;
+	
+	@Autowired
+	protected TaskRetrieveUtils taskRetrieveUtils;
+	
+	@Autowired
+	protected TaskStageMonsterRetrieveUtils taskStageMonsterRetrieveUtils;
+	
+	@Autowired
+	protected TaskStageRetrieveUtils taskStageRetrieveUtils;
+	
+	@Autowired
+	protected StaticDataContainer staticDataContainer;
+	
+	private static final Logger log = LoggerFactory
+			.getLogger(MiscMethods.class);
+	
+	public void reloadAllRareChangeStaticData() {
+		log.info("Reloading rare change static data");
+		achievementRetrieveUtils.reload();
+		bannedUserRetrieveUtils.reload();
+		battleItemRetrieveUtils.reload();
+		boardRetrieveUtils.reload();
+		boardObstacleRetrieveUtils.reload();
+		boardPropertyRetrieveUtils.reload();
+		boosterDisplayItemRetrieveUtils.reload();
+		boosterItemRetrieveUtils.reload();
+		boosterPackRetrieveUtils.reload();
+		//    CityBossRetrieveUtils.reload();
+		//		CityElementsRetrieveUtils.reload(); 
+		//		CityRetrieveUtils.reload();
+		//    ClanBossRetrieveUtils.reload();
+		//    ClanBossRewardRetrieveUtils.reload();
+		clanIconRetrieveUtils.reload();
+		clanEventPersistentRetrieveUtils.reload();
+		clanRaidRetrieveUtils.reload();
+		clanRaidStageRetrieveUtils.reload();
+		clanRaidStageMonsterRetrieveUtils.reload();
+		clanRaidStageRewardRetrieveUtils.reload();
+		eventPersistentRetrieveUtils.reload();
+		fileDownloadRetrieveUtils.reload();
+		//		ExpansionCostRetrieveUtils.reload();
+		goldSaleRetrieveUtils.reload();
+		itemRetrieveUtils.reload();
+		//		LockBoxEventRetrieveUtils.reload();
+		//    MonsterForPvpRetrieveUtils.staticReload();
+		miniJobRetrieveUtils.reload();
+		monsterBattleDialogueRetrieveUtils.reload();
+		monsterLevelInfoRetrieveUtils.reload();
+		monsterRetrieveUtils.reload();
+		obstacleRetrieveUtils.reload();
+		prerequisiteRetrieveUtils.reload();
+		profanityRetrieveUtils.reload();
+		pvpLeagueRetrieveUtils.reload();
+		questJobRetrieveUtils.reload();
+		questJobMonsterItemRetrieveUtils.reload();
+		questRetrieveUtils.reload();
+		researchRetrieveUtils.reload();
+		researchPropertyRetrieveUtils.reload();
+		skillRetrieveUtils.reload();
+		skillPropertyRetrieveUtils.reload();
+		skillSideEffectRetrieveUtils.reload();
+		startupStuffRetrieveUtils.reload();
+		staticUserLevelInfoRetrieveUtils.reload();
+		structureBattleItemFactoryRetrieveUtils.reload();
+		structureClanHouseRetrieveUtils.reload();
+		structureEvoChamberRetrieveUtils.reload();
+		structureHospitalRetrieveUtils.reload();
+		structureLabRetrieveUtils.reload();
+		structureMiniJobRetrieveUtils.reload();
+		structureMoneyTreeRetrieveUtils.reload();
+		structurePvpBoardRetrieveUtils.reload();
+		structureResearchHouseRetrieveUtils.reload();
+		structureResidenceRetrieveUtils.reload();
+		structureResourceGeneratorRetrieveUtils.reload();
+		structureResourceStorageRetrieveUtils.reload();
+		structureRetrieveUtils.reload();
+		structureTeamCenterRetrieveUtils.reload();
+		structureTownHallRetrieveUtils.reload();
+		taskMapElementRetrieveUtils.reload();
+		taskRetrieveUtils.reload();
+		taskStageMonsterRetrieveUtils.reload();
+		taskStageRetrieveUtils.reload();
+		//		TournamentEventRetrieveUtils.reload();
+		//		TournamentEventRewardRetrieveUtils.reload();
+
+		staticDataContainer.reload();
+	}
+	
+}
---
+++ src/main/java/com/lvl6/server/controller/utils/SecretGiftUtils.java
@@ -0,0 +1,83 @@
+package com.lvl6.server.controller.utils;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
+import org.apache.commons.math3.distribution.ChiSquaredDistribution;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.stereotype.Component;
+
+import com.lvl6.info.Item;
+import com.lvl6.info.ItemSecretGiftForUser;
+import com.lvl6.misc.MiscMethods;
+import com.lvl6.properties.ControllerConstants;
+import com.lvl6.retrieveutils.rarechange.ItemRetrieveUtils;
+
+@Component
+@DependsOn("gameServer")
+public class SecretGiftUtils {
+
+	@Autowired
+	protected ItemRetrieveUtils itemRetrieveUtils;
+	
+	private static final Logger log = LoggerFactory
+			.getLogger(MiscMethods.class);
+	
+	
+	public List<ItemSecretGiftForUser> calculateGiftsForUser(
+			String userId, int numGifts, long now) {
+		List<ItemSecretGiftForUser> gifts = new ArrayList<ItemSecretGiftForUser>();
+
+		//random to select an item
+		Random rand = ControllerConstants.RAND;
+
+		//chi random to calculate seconds till collection
+		ChiSquaredDistribution randChi = ControllerConstants.ITEM_SECRET_GIFT_FOR_USER__RANDOM;
+
+		//(round((chisq(df = 4)^3) * 6.5)+329)
+		for (int giftI = 0; giftI < numGifts; giftI++) {
+			float randFloat = rand.nextFloat();
+			Item secretGift = itemRetrieveUtils.nextItem(randFloat);
+
+			ItemSecretGiftForUser isgfu = new ItemSecretGiftForUser();
+			isgfu.setUserId(userId);
+			isgfu.setItemId(secretGift.getId());
+
+			//so the client knows which item came first
+			Date newTime = new Date(now + giftI * 1000);
+			isgfu.setCreateTime(newTime);
+
+			double randDoub = randChi.sample();
+			log.info(String.format("randDoub=%s", randDoub));
+
+			randDoub = Math.pow(randDoub, 3D);
+			log.info(String.format("(randDoub ^ 3)=%s", randDoub));
+
+			double waitTimeSecs = randDoub * 6.5 + 329;
+			log.info(String.format("uncapped waitTimeSecs=%s", waitTimeSecs));
+
+			//(round((chisq(df = 4)^3) * 6.5)+329)
+			waitTimeSecs = Math
+					.min(waitTimeSecs,
+							ControllerConstants.ITEM_SECRET_GIFT_FOR_USER__MAX_SECS_WAIT_TIME);
+			//			waitTimeSecs = Math.max(waitTimeSecs, ControllerConstants
+			//				.ITEM_SECRET_GIFT_FOR_USER__MIN_SECS_WAIT_TIME);
+			log.info(String.format("capped waitTimeSecs=%s", waitTimeSecs));
+
+			isgfu.setSecsTillCollection((int) Math.round(waitTimeSecs));
+
+			log.info(String.format("gift=%s", isgfu));
+
+			gifts.add(isgfu);
+		}
+
+		return gifts;
+	}
+	
+	
+}
