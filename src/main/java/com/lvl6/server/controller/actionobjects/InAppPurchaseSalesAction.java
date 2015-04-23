8c6f928606c3d23fd64fc7ff72666f878ebfa710
Byron Tang <byronyugontang@gmail.cpm>
2015-04-23 15:57:01 -0700

    removed invalidating purchase when checking time of sale

--- src/main/java/com/lvl6/server/controller/actionobjects/InAppPurchaseSalesAction.java
+++ src/main/java/com/lvl6/server/controller/actionobjects/InAppPurchaseSalesAction.java
@@ -192,7 +192,7 @@ public class InAppPurchaseSalesAction {
 			builderCheck();
 		}
 
-		if (duplicateReceipt || !saleIsWithinTimeConstraints()) {
+		if (duplicateReceipt) {
 			log.error("user should be buying more expensive sales package! {}",
 					 user);
 			return false;
@@ -219,22 +219,22 @@ public class InAppPurchaseSalesAction {
 		}
 	}
 
-	//when start and end time both null, it's for starter/builder packs, they dont expire
-	public boolean saleIsWithinTimeConstraints() {
-		Date saleStartTime = salesPackage.getTimeStart();
-		Date saleEndTime = salesPackage.getTimeEnd();
-
-		if(saleStartTime == null && saleEndTime == null) {
-			return true;
-		}
-
-		if((now.getTime() - saleStartTime.getTime() > 0) && (saleEndTime.getTime() - now.getTime() > 0)) {
-			return true;
-		}
-		log.error("sale didn't begin or is over, sale start time is {}, end time is {}",
-				new Timestamp(saleStartTime.getTime()), new Timestamp(saleEndTime.getTime()) );
-		return false;
-	}
+//	//when start and end time both null, it's for starter/builder packs, they dont expire
+//	public boolean saleIsWithinTimeConstraints() {
+//		Date saleStartTime = salesPackage.getTimeStart();
+//		Date saleEndTime = salesPackage.getTimeEnd();
+//
+//		if(saleStartTime == null && saleEndTime == null) {
+//			return true;
+//		}
+//
+//		if((now.getTime() - saleStartTime.getTime() > 0) && (saleEndTime.getTime() - now.getTime() > 0)) {
+//			return true;
+//		}
+//		log.error("sale didn't begin or is over, sale start time is {}, end time is {}",
+//				new Timestamp(saleStartTime.getTime()), new Timestamp(saleEndTime.getTime()) );
+//		return false;
+//	}
 
 	public boolean writeChangesToDB(Builder resBuilder) {
 		boolean success = true;
