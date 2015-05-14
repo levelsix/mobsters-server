56e3134b6ac26b9103707497cff2701f0193f942
Byron Tang <byronyugontang@gmail.cpm>
2015-05-14 13:23:17 -0700

    Revert "sales package changes in startup"
    This reverts commit f47eb51ad11216bd3d0293c416ce9623fd0ff0ee.

--- src/main/scala/com/lvl6/mobsters/services/StartupService.scala
+++ src/main/scala/com/lvl6/mobsters/services/StartupService.scala
@@ -1317,25 +1317,11 @@ class StartupService extends LazyLogging{
     Future{
       timed("StartupService.setSalesForUser"){
           val userSalesValue = user.getSalesValue()
-          val salesLastPurchaseTime = user.getLastPurchaseTime();
+          val newMinPrice = priceForSalesPackToBeShown(userSalesValue);
           val now = new Date
           
-          if(salesLastPurchaseTime == null) {
-            val ts = new Timestamp(now.getTime());
-            updateUtil.updateUserSalesLastPurchaseTime(user.getId(), ts);
-          }
-          else {
-            if(userSalesValue == 0) {
-              if(timeUtils.numDaysDifference(now, salesLastPurchaseTime) > 5) {
-                updateUtil.updateUserSalesValue(user.getId(), 1, now);
-                val userSalesValue = 1;
-              }
-            }
-          }
-          val newMinPrice = priceForSalesPackToBeShown(userSalesValue);
-
           val idsToSalesPackages = salesPackageRetrieveUtil.getSalesPackageIdsToSalesPackages()
-        		  idsToSalesPackages.values().foreach { sp:SalesPackage =>
+          idsToSalesPackages.values().foreach { sp:SalesPackage =>
               if(!sp.getProductId().equalsIgnoreCase(IAPValues.STARTERPACK)
                  && !sp.getProductId().equalsIgnoreCase(IAPValues.BUILDERPACK)
                  && !sp.getProductId().equalsIgnoreCase(IAPValues.STARTERBUILDERPACK))
