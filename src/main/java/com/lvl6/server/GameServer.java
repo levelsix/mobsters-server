968904c77b1640b744b93a50378841ccfacb46f9
Byron Tang <byronyugontang@gmail.cpm>
2015-06-08 14:17:08 -0700

    added logs for clan search reload

--- src/main/java/com/lvl6/server/GameServer.java
+++ src/main/java/com/lvl6/server/GameServer.java
@@ -216,6 +216,7 @@ public class GameServer implements InitializingBean, HazelcastInstanceAware {
 //		reloadRecommendedClans();
 		leaderBoardImpl.reload();
 		hzClanSearch.reload();
+		log.info("finished init");
 	}
 
 	public void reloadRecommendedClans() {
