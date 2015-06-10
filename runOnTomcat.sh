32f88696bffdc057b79a56b4652a1146fe8e1fa1
Arturo Wu Zhou <aweezy90@gmail.com>
2015-06-10 14:04:43 -0700

    tweaking runOnTomcat script

--- runOnTomcat.sh
+++ runOnTomcat.sh
@@ -3,13 +3,13 @@
 tomcatWebappsDir=/Library/Tomcat/webapps
 
 echo "Stopping tomcat"
-/Library/Tomcat/bin/shutdown.sh
+/Library/Tomcat/bin/shutdown.sh; sleep 5
 
 echo "Removing old webapp from $tomcatWebappsDir"
 rm -rf $tomcatWebappsDir/*
 
 echo "Copying new war to $tomcatWebappsDir"
-cp target/mobsters-1.0-SNAPSHOT.war $tomcatWebappsDir/
+cp target/mobsters-server-1.0-SNAPSHOT.war $tomcatWebappsDir/ROOT.war
 
 echo "Starting tomcat"
 /Library/Tomcat/bin/startup.sh
