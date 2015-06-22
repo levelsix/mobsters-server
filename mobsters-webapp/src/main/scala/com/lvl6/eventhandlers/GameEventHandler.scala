298368295e1b8d58663485b04146db6d8f395f4e
bluestix <kelly@dekayd.com>
2015-06-22 15:06:56 -0700

    upgrade spring integration

--- mobsters-webapp/src/main/scala/com/lvl6/eventhandlers/GameEventHandler.scala
+++ mobsters-webapp/src/main/scala/com/lvl6/eventhandlers/GameEventHandler.scala
@@ -123,13 +123,15 @@ trait GameEventHandler extends LazyLogging  {
   def handleMaintenanceMode(parsedEvent:ParsedEvent)={
     val re = parsedEvent.event
     val playerId = re.getPlayerId
-    val user = userRetrieveUtils.getUserById(playerId)
-    if(!user.isAdmin){
+    if(playerId != null && !playerId.isEmpty) {
+      val user = userRetrieveUtils.getUserById(playerId)
+      if(user != null && !user.isAdmin){
+        messagingUtil.sendMaintanenceModeMessage(appMode.getMessageForUsers, playerId, parsedEvent.eventProto.getEventUuid)
+      }
+    }else {
       if(re.isInstanceOf[PreDatabaseRequestEvent] ){
         val udid = re.asInstanceOf[PreDatabaseRequestEvent].getUdid
-        //messagingUtil.sendMaintanenceModeMessageUdid(appMode.getMessageForUsers, udid)
-      }else{
-        //messagingUtil.sendMaintanenceModeMessage(appMode.getMessageForUsers, playerId)
+        messagingUtil.sendMaintanenceModeMessageUdid(appMode.getMessageForUsers, udid, parsedEvent.eventProto.getEventUuid)
       }
     }
   } 
