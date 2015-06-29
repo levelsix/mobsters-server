5c66b7ad1dd29b3fa290c78f12444cbcb311f5b4
bluestix <kelly@dekayd.com>
2015-06-29 00:25:52 -0700

    fix for build error

--- mobsters-webapp/src/main/scala/com/lvl6/eventhandlers/AmqpGameEventHandler.scala
+++ mobsters-webapp/src/main/scala/com/lvl6/eventhandlers/AmqpGameEventHandler.scala
@@ -3,6 +3,7 @@ package com.lvl6.eventhandlers
 import org.springframework.amqp.core.MessageListener
 import org.springframework.amqp.core.Message
 import com.lvl6.server.eventsender.ToClientEvents
+import com.lvl6.server.dynamodb.tables.CachedClientResponse
 
 class AmqpGameEventHandler extends GameEventHandler with MessageListener{
   def onMessage(msg:Message)={
@@ -11,5 +12,9 @@ class AmqpGameEventHandler extends GameEventHandler with MessageListener{
   
   def newToClientEvents(eventUuid:String, playerId:Option[String]):ToClientEvents={
     new ToClientEvents("amqp", eventUuid, playerId)
+  }
+  
+  def sendCachedResponse(cachedResponse:CachedClientResponse)={
+    
   }
 }
